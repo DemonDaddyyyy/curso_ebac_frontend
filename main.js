d4755518a227615c3b7c0afd9964292df22824d2
Demon_Daddyyyy <biancasemedo@gmail.com>
2026-07-30 01:12:11 +0100

    ocorro deus

---
+++ main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', async function () {
+  const nameElement = document.querySelector('#name');
+  const usernameElement = document.querySelector('#username');
+  const avatarElement = document.querySelector('#avatar');
+  const reposElement = document.querySelector('#repos');
+  const followersElement = document.querySelector('#followers');
+  const followingElement = document.querySelector('#following');
+  const linkElement = document.querySelector('#link');
+
+  try {
+    const res = await fetch('https://api.github.com/users/DemonDaddyyyy');
+
+    if (!res.ok) {
+      throw new Error('Erro ao buscar usuário.');
+    }
+
+    const json = await res.json();
+
+    nameElement.innerText = json.name;
+    usernameElement.innerText = json.login;
+    avatarElement.src = json.avatar_url;
+    followingElement.innerText = json.following;
+    followersElement.innerText = json.followers;
+    reposElement.innerText = json.public_repos;
+    linkElement.href = json.html_url;
+
+  } catch (erro) {
+    console.log(erro.message);
+  }
+});
